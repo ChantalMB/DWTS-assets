70f88dd23a78f8cca5b45594e33577e696907fba
ChantalMB <chantalbrousseau@rogers.com>
2023-08-15 20:06:58 -0400

    repo init

---
+++ src/App.svelte
@@ -0,0 +1,29 @@
+<script>
+	import { onMount } from 'svelte';
+	import OpenSeadragon from 'openseadragon';
+	import Icon from '@iconify/svelte';
+
+
+	onMount( async () => {
+
+		let viewer = OpenSeadragon({
+			id: 'openseadragon',
+			prefixUrl: "//openseadragon.github.io/openseadragon/images/",
+			maxZoomLevel: 10,
+			showNavigator:  true,
+			tileSources: {
+				type: 'image',
+				url: "/images/manchester1926.jpg",
+			},
+		});
+	});
+</script>
+
+<div id='openseadragon'></div>
+
+<style>
+	#openseadragon {
+		height: 100%;
+		width: 100%;
+	}
+</style>
